3ac2f98b2a083f5b8ecfb70415c20038e6285542
Daniel Dzivý <60179342+ddzivy@users.noreply.github.com>
2020-03-17 16:43:59 +0200

    Add files via upload

---
+++ src/main/java/com/example/e1c2_dzivy/E1c2DzivyApplication.java
@@ -0,0 +1,13 @@
+package com.example.e1c2_dzivy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1c2DzivyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E1c2DzivyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e1c2_dzivy/E1c2DzivyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e1c2_dzivy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1c2DzivyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
